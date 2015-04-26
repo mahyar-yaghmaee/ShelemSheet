15ebeb731aa9e2fe0758c503acce455435afefcd
Mahyar Yaghmaee <mahyar.yaghmaee@gmail.com>
2015-04-26 12:05:54 -0700

    Choose player: Add ResetChoosePlayer - Reset Button works!

--- app/src/main/java/com/android/mahyar/shelemsheet/choosePlayers.java
+++ app/src/main/java/com/android/mahyar/shelemsheet/choosePlayers.java
@@ -4,14 +4,43 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 public class choosePlayers extends ActionBarActivity {
+
+    private Button mResetButton;
+    private Button mSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_players);
+
+        mResetButton = (Button)findViewById(R.id.reset_button);
+        mResetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText player1text = (EditText)findViewById(R.id.editText_player1);
+                EditText player2text = (EditText)findViewById(R.id.editText_player2);
+                EditText player3text = (EditText)findViewById(R.id.editText_player3);
+                EditText player4text = (EditText)findViewById(R.id.editText_player4);
+                EditText handpoint = (EditText)findViewById(R.id.editText_handPoint);
+                EditText maxPoints = (EditText)findViewById(R.id.editText_totalPoint);
+                new choosePlayerReset(player1text, player2text, player3text, player4text, handpoint, maxPoints);
+            }
+        });
+
+        mSubmitButton = (Button)findViewById(R.id.submit_button);
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 
 
